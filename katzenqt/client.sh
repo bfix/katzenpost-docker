4ca0fdcbe3cd2197f9c096bcdd254d2bddec25f4
Bernd Fix <brf@hoi-polloi.org>
2025-12-24 16:42:57 +0100

    Working configuration (except for some Nvidia cards).

--- katzenqt/client.sh
+++ katzenqt/client.sh
@@ -22,9 +22,10 @@ esac
 
 docker run ${MODE} \
     --name ${CNT} -h ${CNT} \
-	--ipc=host \
+	--device /dev/dri \
 	-e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
 	-e DISPLAY=${DISPLAY} \
+   	-e QT_QPA_PLATFORM=xcb \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /run/user/$(id -u)/pulse:/run/user/1000/pulse \
     -v $(pwd)/conf:/home/user/katzenqt/config \
